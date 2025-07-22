b6147ab230bb30bba18e71334f1d3ea368c51115
sgronwold <89616328+sgronwold@users.noreply.github.com>
2025-07-21 19:35:46 -0500

    refactor of all-team option for ical

--- calendarmaker/views.py
+++ calendarmaker/views.py
@@ -37,43 +37,50 @@ def ical(req:HttpRequest, sport:str, league:str):
     l = League.objects.get(sport__iexact=sport, league=league)
     league = l
 
+    # games to add to the calendar
+    gamelist = []
     if league != None:
         # team abbr's
         teams = req.GET.get("teams")
-        if teams == None:
-            # then get all the teams
-            teams = ",".join([t.tricode for t in Team.objects.filter(league=league).all()])
-        teams = teams.split(",")
+        if teams != None:
+            teams = teams.split(",")
+        
+            for t in teams:
+                team = Team.objects.get(tricode__iexact=t, league=league)
 
-        for t in teams:
-            team = Team.objects.get(tricode__iexact=t, league=league)
+                # default date radius is one year in the past, one year in the future
+                # TODO make this customizable
+                DATE_RADIUS = 365#days
+                START = dt.now(tz=pytz.utc) - td(days=DATE_RADIUS)
+                END = dt.now(tz=pytz.utc) + td(days=DATE_RADIUS)
 
-            # default date radius is one year in the past, one year in the future
-            # TODO make this customizable
-            DATE_RADIUS = 365#days
-            START = dt.now(tz=pytz.utc) - td(days=DATE_RADIUS)
-            END = dt.now(tz=pytz.utc) + td(days=DATE_RADIUS)
+                # update the database
+                if league not in nextUpdate.keys():
+                    nextUpdate[league] = Lock()
+                if nextUpdate[league].acquire(timeout=0):
+                    helpers.loadScheduleByDateRange(league, START, END)
 
-            # update the database
-            if league not in nextUpdate.keys():
-                nextUpdate[league] = Lock()
-            if nextUpdate[league].acquire(timeout=0):
-                helpers.loadScheduleByDateRange(league, START, END)
+                    # release the lock after a minute i.e. 60 seconds
+                    Thread(target=release_thread_after_delay, args=(nextUpdate[league], 60))
 
-                # release the lock after a minute i.e. 60 seconds
-                Thread(target=release_thread_after_delay, args=(nextUpdate[league], 60))
+                for game in Game.objects.filter(Q(start__range=(START,END)) & (Q(hometeam=team)|Q(awayteam=team))):
+                    gamelist.append(game)
+        else:
+            # get all teams' games, if not specified
+            for game in Game.objects.filter(Q(start__range=(START,END))):
+                gamelist.append(game)
 
-            for game in Game.objects.filter(Q(start__range=(START,END)) & (Q(hometeam=team)|Q(awayteam=team))):
-                event = Event()
-                event.add('summary', '%s @ %s'%(game.awayteam.name, game.hometeam.name))
-                event.add('description', 'Watch on: %s'%(", ".join([n.name for n in game.networks.all()])))
-                event.add('dtstart', game.start)
-                event.add('dtend', game.start + td(hours=3))
-                event.add('UID', uuid.uuid4().__str__())
+    for game in gamelist:
+        event = Event()
+        event.add('summary', '%s @ %s'%(game.awayteam.name, game.hometeam.name))
+        event.add('description', 'Watch on: %s'%(", ".join([n.name for n in game.networks.all()])))
+        event.add('dtstart', game.start)
+        event.add('dtend', game.start + td(hours=3))
+        event.add('UID', uuid.uuid4().__str__())
 
-                event.add('DTSTAMP', dt.now())
+        event.add('DTSTAMP', dt.now())
 
-                c.add_component(event)
+        c.add_component(event)
 
         buffer = io.BytesIO()
         buffer.write(c.to_ical())
