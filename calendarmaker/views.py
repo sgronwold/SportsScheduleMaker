065e4247fe15ea4bc8503a2fe2dfb3d8445cd69d
sgronwold <89616328+sgronwold@users.noreply.github.com>
2025-07-21 19:51:48 -0500

    bug fix in all-team option in ical

--- calendarmaker/views.py
+++ calendarmaker/views.py
@@ -42,17 +42,16 @@ def ical(req:HttpRequest, sport:str, league:str):
     if league != None:
         # team abbr's
         teams = req.GET.get("teams")
+        # default date radius is one year in the past, one year in the future
+        # TODO make this customizable
+        DATE_RADIUS = 365#days
+        START = dt.now(tz=pytz.utc) - td(days=DATE_RADIUS)
+        END = dt.now(tz=pytz.utc) + td(days=DATE_RADIUS)
         if teams != None:
             teams = teams.split(",")
         
             for t in teams:
                 team = Team.objects.get(tricode__iexact=t, league=league)
-
-                # default date radius is one year in the past, one year in the future
-                # TODO make this customizable
-                DATE_RADIUS = 365#days
-                START = dt.now(tz=pytz.utc) - td(days=DATE_RADIUS)
-                END = dt.now(tz=pytz.utc) + td(days=DATE_RADIUS)
 
                 # update the database
                 if league not in nextUpdate.keys():
@@ -67,27 +66,36 @@ def ical(req:HttpRequest, sport:str, league:str):
                     gamelist.append(game)
         else:
             # get all teams' games, if not specified
-            for game in Game.objects.filter(Q(start__range=(START,END))):
+            for game in Game.objects.filter(Q(start__range=(START,END))).all():
                 gamelist.append(game)
 
+    threads = []
     for game in gamelist:
-        event = Event()
-        event.add('summary', '%s @ %s'%(game.awayteam.name, game.hometeam.name))
-        event.add('description', 'Watch on: %s'%(", ".join([n.name for n in game.networks.all()])))
-        event.add('dtstart', game.start)
-        event.add('dtend', game.start + td(hours=3))
-        event.add('UID', uuid.uuid4().__str__())
+        t = Thread(target=add_calendar_component, args=(c, game))
+        t.start()
+        threads.append(t)
+    
+    for t in threads:
+        t.join()
 
-        event.add('DTSTAMP', dt.now())
+    buffer = io.BytesIO()
+    buffer.write(c.to_ical())
+    buffer.seek(0)
+    
 
-        c.add_component(event)
+    return FileResponse(buffer, content_type='text/calendar')
 
-        buffer = io.BytesIO()
-        buffer.write(c.to_ical())
-        buffer.seek(0)
-        
+def add_calendar_component(c:Calendar, game:Game):
+    event = Event()
+    event.add('summary', '%s @ %s'%(game.awayteam.name, game.hometeam.name))
+    event.add('description', 'Watch on: %s'%(", ".join([n.name for n in game.networks.all()])))
+    event.add('dtstart', game.start)
+    event.add('dtend', game.start + td(hours=3))
+    event.add('UID', uuid.uuid4().__str__())
 
-        return FileResponse(buffer, content_type='text/calendar')
+    event.add('DTSTAMP', dt.now())
+
+    c.add_component(event)
 
 def release_thread_after_delay(lock:Lock, delay_sec:int):
     sleep(delay_sec)
